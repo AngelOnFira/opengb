a3d212025d5ba171513efc10a22a24f2e57ff91a
Nathan Flurry <git@nathanflurry.com>
2024-02-08 19:21:18 -0800

    test ts-json-schema-generator

--- engine/builder/src/deps.ts
+++ engine/builder/src/deps.ts
@@ -1,3 +1,3 @@
 export * as tjs from 'npm:typescript-json-schema@^0.62.0';
+export * as tsjsg from 'npm:ts-json-schema-generator@^1.5.0';
 export { glob } from 'npm:glob@^10.3.10';
-
--- engine/builder/src/schema.ts
+++ engine/builder/src/schema.ts
@@ -1,41 +1,58 @@
 import * as path from "https://deno.land/std/path/mod.ts";
-import { glob, tjs } from './deps.ts';
+import { glob, tsjsg } from './deps.ts';
 import { Registry } from '../../registry/src/index.ts';
 
-// const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
 export async function compileSchema(registry: Registry) {
     for (let module of registry.modules.values()) {
         for (let script of module.scripts.values()) {
             console.log("Generating schema", script.path);
 
-            const validateConfig = {
+            // const validateConfig = {
+            //     topRef: true,
+            //     required: true,
+            //     strictNullChecks: true,
+            //     noExtraProps: true,
+            //     esModuleInterop: true,
+
+            //     // TODO: Is this needed?
+            //     include: [script.path],
+
+            //     // TODO: Figure out how to work without this? Maybe we manually validate the request type exists?
+            //     ignoreErrors: true,
+            // };
+
+            // const program = tjs.getProgramFromFiles([script.path], {
+            //     target: "es2015",
+            //     esModuleInterop: true,
+            //     allowImportingTsExtensions: true,
+            // });
+
+            // const requestSchema = tjs.generateSchema(program, "Request", validateConfig);
+            // if (requestSchema === null) throw new Error("Failed to generate request schema for " + script.path);
+            // script.requestSchema = requestSchema;
+
+            // const responseSchema = tjs.generateSchema(program, "Response", validateConfig);
+            // if (responseSchema === null) throw new Error("Failed to generate response schema for " + script.path);
+            // script.responseSchema = responseSchema;
+
+            let genConfig = {
+                path: script.path,
+                tsconfig: path.resolve(__dirname, "..", "..", "..", "tsconfig.json"),
                 topRef: true,
-                required: true,
-                strictNullChecks: true,
-                noExtraProps: true,
-                esModuleInterop: true,
-
-                // TODO: Is this needed?
-                include: [script.path],
-
-                // TODO: Figure out how to work without this? Maybe we manually validate the request type exists?
-                ignoreErrors: true,
             };
 
-            const program = tjs.getProgramFromFiles([script.path], {
-                target: "es2015",
-                esModuleInterop: true,
-                allowImportingTsExtensions: true,
-            });
-
-            const requestSchema = tjs.generateSchema(program, "Request", validateConfig);
-            if (requestSchema === null) throw new Error("Failed to generate request schema for " + script.path);
-            script.requestSchema = requestSchema;
-
-            const responseSchema = tjs.generateSchema(program, "Response", validateConfig);
-            if (responseSchema === null) throw new Error("Failed to generate response schema for " + script.path);
-            script.responseSchema = responseSchema;
+            let requestSchema = tsjsg.createGenerator(Object.assign({
+                type: "Request"
+            }, genConfig)).createSchema("Request");
+            console.log('schema', requestSchema);
+            script.requestSchema = requestSchema as any;
+        
+            let responseSchema = tsjsg.createGenerator(Object.assign({
+                type: "Response"
+            }, genConfig)).createSchema("Response");
+            script.responseSchema = responseSchema as any;
         }
     }
 }
